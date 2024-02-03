2deaf22f9fbed78f140417fe9888456d4f18e4fc
ekaterina <sychevaee@icloud.com>
2024-02-04 03:58:43 +1100

    Completed task on electronic_shop_project

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,15 +1,38 @@
 from src.item import Item
+import pytest
+from config import PATH
 
-data = Item("Смартфон", 10000, 20)
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
 
 
-def test_calculate_total_price():
-    assert data.calculate_total_price() == 200000
-    assert data.quantity == 20
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+    assert item.quantity == 20
 
 
-def test_apply_discount():
-    data.pay_rate = 0.8
-    data.apply_discount()
-    assert data.price == 8000
+def test_apply_discount(item):
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000
 
+
+def test_name():
+    """Проверка длины наименования товара (не более 10 символов)"""
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.name == "Смартфон"
+    item1.name = "Посудомойка"
+    assert item1.name == "Посудомойк"
+
+
+def test_instantiate_from_csv():
+    """Проверка добавления экземпляров класс из .csv файла"""
+    Item.instantiate_from_csv(PATH)
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    """Проверка возвращения числа из строки"""
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('2.5') == 2
