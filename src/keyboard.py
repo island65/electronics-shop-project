2c51c3495b4341ca8dd02f922abdaf37ec56a3e0
ekaterina <sychevaee@icloud.com>
2024-02-16 16:44:34 +1100

    Completed Task 5

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,18 +1,22 @@
 from src.item import Item
 
-class Mixin:
-    _language: str = 'EN'
 
-    def change_lang(self, _language) -> None:
-        if self._language == "EN":
-            self._language = "RU"
-        else:
-            self._language = 'EN'
+class Mixin:
+    def __init__(self):
+        self._language = 'EN'
 
     @property
     def language(self) -> str:
         return self._language
 
-class Keyboard (Item, Mixin):
-    pass
+    def change_lang(self) -> None:
+        if self._language == "EN":
+            self._language = "RU"
+        else:
+            self._language = 'EN'
 
+
+class Keyboard (Item, Mixin):
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        Mixin.__init__(self)
